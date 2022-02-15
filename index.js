ea9a154a26cb13b0bdf41e409c172aa069c09eb2
yomZsamora <samora.y@adzumi.co.ke>
2022-02-15 22:40:07 +0300

    Function populates a field properties from the elements from passed array of employee details.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+let createEmployeeRecord = (employeeCard) => {
+   return {
+      firstName: employeeCard[0],
+      familyName: employeeCard[1],
+      title: employeeCard[2],
+      payPerHour: employeeCard[3],
+      timeInEvents: [],
+      timeOutEvents: []
+   }
+}
+
+let createEmployeeRecords = (employeesCardsData) => {
+   return employeesCardsData.map((employeeInfo) => {
+      return createEmployeeRecord(employeeInfo)
+   })
+}
+
+let createTimeInEvent = (employeeRecord, dateStamp) => {
+   
+}
