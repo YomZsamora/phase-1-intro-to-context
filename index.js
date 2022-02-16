4a7f88f10c0ece47f0271153a6d4fb6eb24a0393
yomZsamora <samora.y@adzumi.co.ke>
2022-02-16 21:42:00 +0300

    Converts each emploee Array into an employee record using createEmployeeRecord and accumulates it to a new Array.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Your code here
+// Function populates a field properties from the elements from passed array of employee details
 let createEmployeeRecord = (employeeCard) => {
    return {
       firstName: employeeCard[0],
@@ -10,6 +10,7 @@ let createEmployeeRecord = (employeeCard) => {
    }
 }
 
+// Converts each emploee Array into an employee record using createEmployeeRecord and accumulates it to a new Array
 let createEmployeeRecords = (employeesCardsData) => {
    return employeesCardsData.map((employeeInfo) => {
       return createEmployeeRecord(employeeInfo)
@@ -17,5 +18,48 @@ let createEmployeeRecords = (employeesCardsData) => {
 }
 
 let createTimeInEvent = (employeeRecord, dateStamp) => {
-   
+   let [date, hour] = dateStamp.split(' ');
+   employeeRecord.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date
+   })
+   return employeeRecord
+}
+
+let createTimeOutEvent = (employeeRecord, dateStamp) => {
+   let [date, hour] = dateStamp.split(' ');
+   employeeRecord.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date
+   })
+   return employeeRecord
+}
+
+let hoursWorkedOnDate = (employeeRecord, dateOfTheForm) => {
+   let clockIn = employeeRecord.timeInEvents.find((e) => e.date == dateOfTheForm).hour
+   let clockOut = employeeRecord.timeOutEvents.find((e) => e.date == dateOfTheForm).hour
+   return (clockOut - clockIn)/100
+}
+
+let wagesEarnedOnDate = (employeeRecord, dateOfTheForm) => {
+   const payPerHour = parseInt(employeeRecord.payPerHour)
+   const hoursWorked = hoursWorkedOnDate(employeeRecord, dateOfTheForm)
+   return payPerHour * hoursWorked
+}
+
+let allWagesFor = (employeeRecord) => {
+   let employeeWages = []
+   let datesWorked = employeeRecord.timeInEvents.map((e) => e.date)
+   for (let date of datesWorked) {
+      employeeWages.push(wagesEarnedOnDate(employeeRecord, date))
+   }
+   return employeeWages.reduce((total, wage) => total + wage)
+}
+
+let calculatePayroll = (AllEmployeeRecords) => {
+   return AllEmployeeRecords.reduce((previoutAmount, employee) => {
+      return previoutAmount + allWagesFor(employee)
+  }, 0)
 }
